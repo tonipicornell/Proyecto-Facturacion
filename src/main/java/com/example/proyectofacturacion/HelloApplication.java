11c79bc59e7bd4cb175bb318e05babd5b2e5a666
tonipicornell <tpicornell59@gmail.com>
2025-01-31 20:25:07 +0100

    Aumento pantalla

--- src/main/java/com/example/proyectofacturacion/HelloApplication.java
+++ src/main/java/com/example/proyectofacturacion/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("pantalla-inicial.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 750);
         stage.setTitle("Facturación");
         stage.setScene(scene);
         stage.show();
